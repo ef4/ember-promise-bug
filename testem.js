f277be7ea30e417c88dbf31cbf98a169f9e02b96
Boris Petrov <boris_petrov@live.com>
2018-10-01 12:35:55 +0300

    Fix running tests on Linux

--- testem.js
+++ testem.js
@@ -2,10 +2,10 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    process.platform === 'win32' ? 'Chrome' : 'Chromium',
   ],
   launch_in_dev: [
-    'Chrome'
+    process.platform === 'win32' ? 'Chrome' : 'Chromium',
   ],
   browser_args: {
     Chrome: {
