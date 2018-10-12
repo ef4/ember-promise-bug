727de4e605a96cec0ff5f8a8724404a498ab61c5
Edward Faulkner <ef@alum.mit.edu>
2018-10-12 11:29:03 +0200

    make test non-racy

--- app/helpers/foo.js
+++ app/helpers/foo.js
@@ -18,6 +18,6 @@ export default Helper.extend({
       return this.recompute();
     });
 
-    return '';
+    return 'loading';
   }
 })
--- tests/integration/helpers/foo-test.js
+++ tests/integration/helpers/foo-test.js
@@ -1,15 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Helpers | foo', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    assert.expect(1);
+    assert.expect(2);
 
+    // this only waits for the render to complete
     await render(hbs`{{if true (foo)}}`);
+
+    // which allows us to assert on the loading state, if we want to
+    assert.equal(this.element.textContent.trim(), 'loading');
+
+    // this waits for everything scheduled within Ember to finish. In our case,
+    // it will wait for the `later` in our foo helper to run (and it will also
+    // wait for the resulting re-render to happen.
+    await settled();
 
     assert.equal(this.element.textContent.trim(), 'text!');
   });
